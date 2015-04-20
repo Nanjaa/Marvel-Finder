9454054deb12c7d11bfbecfab62919c4eb16681e
Stephanie Olfert <steph.o-91@hotmail.com>
2015-04-20 18:13:44 -0500

    Worked a bit more on the project, using a tutorial as a guide

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 $(document).ready(function() {
 
-// begin scripts
+// begin
 
-var getInformation = function() {
-	var comic = $('#search').val();
 
-	if(comic == '') {
-		$('.holder').html("<h1 class='loading'>There's nothing in the search box.</h1>");
+var getPoster = function() {
+	var film = $('#term').val();
+	console.log(film);
+	if(film == '') {
+		$('#poster').html("<h2 class='loading'>Fuck you</h2>")
 	}
 	else {
-		$('.holder').html("<h1 class='loading'>Your results are on the way</h1>");
-		
-		$.getJSON("http://gateway.marvel.com:80/v1/public/comics?title=" + comic + "&apikey=7e74289abba6ba60c0ec85bc595e7416", function(json) {
-			console.log(json);
+		$('#poster').html("<h2 class='loading'>Loading</h2>");
+		$.get("http://gateway.marvel.com/v1/public/series?title=" + film + "&apikey=7e74289abba6ba60c0ec85bc595e7416", function(json) {
+			if (json != "Nothing found.") {
+				$('#poster').html('<h2 class="loading">Here are your details</h2><p>(json[0].data.results[0].title)</p>')
 			}
-		})
+			else {
+				$.get("http://gateway.marvel.com/v1/public/series?title=hulk&apikey=7e74289abba6ba60c0ec85bc595e7416", function(json) {
+					console.log(json);
+					$('#poster').html('<h2 class="loading">Unfortunately I fucked up</h2>');
+				});
+			}
+		});
 	}
+	return false;
+
 }
 
-$('#searchButton').click(getInformation);
-$('#search').keyup(function(event) {
+$('#search').click(getPoster);
+$('#term').keyup(function(event) {
 	if(event.keyCode == 13) {
-		getInformation();
+		getPoster();
 	}
 });
 
-// end scripts
+
+
+
+
+
+// end
 
 });
