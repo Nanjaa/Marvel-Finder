4ef1d54d1d99c0d749580c8741d0edd87cc44691
Stephanie Olfert <steph.o-91@hotmail.com>
2015-04-22 03:43:11 -0500

    I like having a test page

--- test.js
+++ test.js
@@ -2,51 +2,48 @@ $(document).ready(function() {
 
 // begin
 
+$('#term').select();
+var script = document.createElement('script');
 
-var getPoster = function() {
-	var film = $('#term').val();
-	console.log(film);
-	if(film == '') {
-		$('#poster').html("<h2 class='loading'>Fuck you</h2>")
+$('#search').click(function(inp) {
+	var series = $('#term').val();
+	if(series == '') {
+		$('#loading').text('PLEASE ENTER SOMETHING, COCKHEAD');
 	}
 	else {
-		$('#poster').html("<h2 class='loading'>Loading</h2>");
-		$.get("http://gateway.marvel.com/v1/public/series?title=" + film + "&apikey=7e74289abba6ba60c0ec85bc595e7416", function(json) {
-			if (json != "Nothing found.") {
-				var marvel = json
-
-				var name = json.data.results[0].title;
-				var thumbnail = marvel.data.results[0].thumbnail.path + '.' + marvel.data.results[0].thumbnail.extension;
-
-
-					$('#comicTitle').text(name);
-					$('#comicStart').text(marvel.data.results[0].startYear + " - " + marvel.data.results[0].endYear);
-					$('#comicThumb').attr('src', thumbnail);
-					$('#comicDescription').text(marvel.data.results[0].description);
-					$('#comicAvail').text(marvel.data.results[0].comics.available + "issues available");
-					$('#content').text("this is a test");
-
-
-			}
-			else {
-				$.get("http://gateway.marvel.com/v1/public/series?title=hulk&apikey=7e74289abba6ba60c0ec85bc595e7416", function(json) {
-					console.log(json);
-					$('#poster').html('<h2 class="loading">Unfortunately I fucked up</h2>');
-				});
-			}
-		});
+		script.src = 'http://gateway.marvel.com:80/v1/public/series?title=' + series + '&apikey=7e74289abba6ba60c0ec85bc595e7416';
 	}
-	return false;
+	scrit = null;
+	$(script).remove();
+	script = document.createElement('script');
+})
 
-}
-
-$('#search').click(getPoster);
-$('#term').keyup(function(event) {
-	if(event.keyCode == 13) {
-		getPoster();
-	}
 });
 
+// get data from api
+
+var getSeries = function(marvel) {
+	if('error' in marvel.data) {
+		$('#loading').text('INVALID SEARCH');
+		return;
+	}
+	else {
+		$('#loading').text('');
+		$('#title').text(name);
+		$('#comicThumb').attr('src', thumbnail);
+		$('#comicDescription').text(marvel.data.results[0].description);
+		$('#comicAvail').text(marvel.data.results[0].comics.available + " issues available");
+
+	}
+} 
+
+
+$('#search').click(getSeries);
+$('#term').keyup(function(event) {
+	if(event.keyCode == 13) {
+		getSeries();
+	}
+});
 
 
 
@@ -55,33 +52,3 @@ $('#term').keyup(function(event) {
 // end
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-<div class="container">
-  <section id="fetch">
-    <input type="text" placeholder="search" id="term" />
-    <button id="search">Go</button>
-  </seciton>
-  <section id="comicInfo">
-    <h1 id="loading"></h1>
-    <h1 id="comicTitle"></h1>
-    <p id="comicTitleSmall"></p>
-    <p id="comicDescription"></p>
-    <p id="comicStart"></p>
-    <p id="comicEnd"></p>
-    <img id="comicThumb">
-    <p id="comicAvail"></p>
-    <p id="content"></p>
-  </section>
-</div>
