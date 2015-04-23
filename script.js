61257378b83be62039f94aec8b41da30df7fbff0
Stephanie Olfert <steph.o-91@hotmail.com>
2015-04-23 00:49:13 -0500

    cleaned up the super messy script at least a little bit

--- script.js
+++ script.js
@@ -7,22 +7,23 @@ $(document).ready(function() {
 function browseFunction() {
 	$('#mainPage').hide();
 	$('#browseComics').fadeIn('slow');	
-}
+};
 function homeFunction() {
 	$('#browseComics').hide();
 	$('#mainPage').fadeIn('slow');	
-}
+};
 
 $('#browse').click(browseFunction);
 $('#home').click(homeFunction);
 
+function showGif() {
+	$('#loadingGif').toggle();
+};
+
 // ****************************************** MAIN PAGE *****************************************************************
 
 $('#spiderman').click(browseFunction);
-$('#spiderman').click(function() {
-})
-$('#spiderman').hover(function() {
-})
+
 // ****************************************** SEARCH COMICS *************************************************************
 
 var getSeries = function() {
@@ -33,13 +34,13 @@ var getSeries = function() {
 	}
 	else {
 		$('#loading').text('Loading');
-		$('#loadingGif').show();
+		showGif();
 // retrieve the seried information from the API
 		$.get("http://gateway.marvel.com/v1/public/series?title=" + series + "&apikey=7e74289abba6ba60c0ec85bc595e7416", function(json) {
 // respond if there is no data
 			if(typeof json.data.results[0] == 'undefined') {
 				$('#loading').hide();
-				$('#loadingGif').hide();
+				showGif();
 				$('#title').text('No Results Found');
 				$('#comicDescription').text("We're sorry, but we could not find anything under that name. Check your spelling, and try again!");
 			}
@@ -47,10 +48,35 @@ var getSeries = function() {
 			else {
 				var marvel = json
 				$('#loading').text('Please choose a series above');
-				$('#loadingGif').hide();
+				showGif();
 				$('.optionOne').text(marvel.data.results[0].title);
 				$('.optionTwo').text(marvel.data.results[1].title);
 				$('.optionThree').text(marvel.data.results[2].title);
+// sets up the function for later 
+				function getInfo() {
+					$('#title').text(marvel.data.results[0].title);
+					$('#comicDescription').text(marvel.data.results[0].description);
+					$('#thumbnail').attr('src', marvel.data.results[0].thumbnail.path + '.' + marvel.data.results[0].thumbnail.extension);
+					$('#rating').text("rating: " + marvel.data.results[0].rating);
+					$('#url').attr('href', marvel.data.results[0].urls[0].url);
+					$('#urlText').text('View this series at Marvel.com');
+					$('#creatorsNumber').text('creators: ' + marvel.data.results[0].creators.available);
+					$('#creatorsNumber').hover(function() {
+						$('#creators').show();
+					});
+					console.log(marvel.data.results[0].creators.available);
+					var creatorsList = marvel.data.results[0].creators.items;
+
+					var findCreators = function() { 
+						for(var i=0; i<creatorsList.length; i++) {
+						$('#creators').append('<p>' + creatorsList[i].name + '</p>');
+						};
+					};
+					findCreators();
+
+					$('#loading').hide();
+				};
+
 // retrieve the information for option one
 				$('.optionOne').click(function() {
 					$('.optionOne').hide();
@@ -61,27 +87,7 @@ var getSeries = function() {
 					$.get("http://gateway.marvel.com:80/v1/public/series/" + ID + "?apikey=7e74289abba6ba60c0ec85bc595e7416", function(json) {
 						$('loadingGif').hide();
 						var marvel = json
-						$('#title').text(marvel.data.results[0].title);
-						$('#comicDescription').text(marvel.data.results[0].description);
-						$('#thumbnail').attr('src', marvel.data.results[0].thumbnail.path + '.' + marvel.data.results[0].thumbnail.extension);
-						$('#rating').text("rating: " + marvel.data.results[0].rating);
-						$('#url').attr('href', marvel.data.results[0].urls[0].url);
-						$('#urlText').text('View this series at Marvel.com');
-						$('#creatorsNumber').text('creators: ' + marvel.data.results[0].creators.available);
-						$('#creatorsNumber').hover(function() {
-							$('#creators').show();
-						});
-						console.log(marvel.data.results[0].creators.available);
-						var creatorsList = marvel.data.results[0].creators.items;
-
-						var findCreators = function() { 
-							for(var i=0; i<creatorsList.length; i++) {
-							$('#creators').append('<p>' + creatorsList[i].name + '</p>');
-							};
-						};
-						findCreators();
-
-						$('#loading').hide();
+						getInfo();
 					});
 				});
 // retrieve the information for option two
@@ -91,27 +97,9 @@ var getSeries = function() {
 					$('.optionThree').hide();
 					var ID = json.data.results[1].id;
 					$.get("http://gateway.marvel.com:80/v1/public/series/" + ID + "?apikey=7e74289abba6ba60c0ec85bc595e7416", function(json) {
+						$('loadingGif').hide();
 						var marvel = json
-						$('#title').text(marvel.data.results[1].title);
-						$('#comicDescription').text(marvel.data.results[1].description);
-						$('#thumbnail').attr('src', marvel.data.results[1].thumbnail.path + '.' + marvel.data.results[0].thumbnail.extension);
-						$('#rating').text("rating: " + marvel.data.results[1].rating);
-						$('#url').attr('href', marvel.data.results[1].urls[0].url);
-						$('#urlText').text('View this series at Marvel.com');
-						$('#creatorsNumber').text('creators: ' + marvel.data.results[1].creators.available);
-						$('#creatorsNumber').hover(function() {
-							$('#creators').show();
-						})
-						var creatorsList = marvel.data.results[1].creators.items;
-
-						var findCreators = function() { 
-							for(var i=0; i<creatorsList.length; i++) {
-							$('#creators').append('<p>' + creatorsList[i].name + '</p>');
-							};
-						};
-						findCreators();
-
-						$('#loading').hide();
+						getInfo();
 					});
 				});
 // retrieve the information for option three
@@ -122,28 +110,7 @@ var getSeries = function() {
 					var ID = json.data.results[2].id;
 					$.get("http://gateway.marvel.com:80/v1/public/series/" + ID + "?apikey=7e74289abba6ba60c0ec85bc595e7416", function(json) {
 						var marvel = json
-						$('#title').text(marvel.data.results[2].title);
-						$('#comicDescription').text(marvel.data.results[2].description);
-						$('#thumbnail').attr('src', marvel.data.results[2].thumbnail.path + '.' + marvel.data.results[0].thumbnail.extension);
-						$('#rating').text("rating: " + marvel.data.results[2].rating);
-						$('#url').attr('href', marvel.data.results[2].urls[0].url);
-						$('#urlText').text('View this series at Marvel.com');
-						$('#creatorsNumber').text('creators: ' + marvel.data.results[2].creators.available);
-						$('#creatorsNumber').hover(function() {
-							$('#creators').show();
-						})
-						var creatorsList = marvel.data.results[2].creators.items;
-
-						var creatorsList = marvel.data.results[2].creators.items;
-
-						var findCreators = function() { 
-							for(var i=0; i<creatorsList.length; i++) {
-							$('#creators').append('<p>' + creatorsList[i].name + '</p>');
-							};
-						};
-						findCreators();
-
-						$('#loading').hide();
+						getInfo();
 					});
 // EVERYTHING BELOW THIS LINE IS HAPPY AND GOOD DO NOT INTERFERE
 				});
